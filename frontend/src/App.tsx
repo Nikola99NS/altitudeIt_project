ac92064bd2eb1a975b5318ff7dc87d8f787a3842
Nikola99NS <nikolasimidzija1@gmail.com>
2024-11-04 16:17:04 +0100

    setup backend, frontend and db

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="text-center bg-blue-500 text-white p-4">
+      <h1 className="text-2xl font-bold">Welcome to my App!</h1>
+    </div>
+  );
+}
+
+export default App;
